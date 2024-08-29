4fb25438348d90c27d84ad94a080daf0828675aa
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-08-29 06:47:00 -0300

    feat: Add BackgroundWidget class for displaying images with credits

---
+++ lib/helpers/background_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class BackgroundWidget extends StatelessWidget {
+  final String image;
+  final String credit;
+  const BackgroundWidget(
+      {super.key, required this.image, required this.credit});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomRight,
+      children: [
+        Image.asset(
+          image,
+          fit: BoxFit.fitHeight,
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+          filterQuality: FilterQuality.high,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.5),
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              credit,
+              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
