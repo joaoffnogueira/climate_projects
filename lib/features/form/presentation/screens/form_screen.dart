8966db367259e29d92145d15cbc83fb3ab1cfd91
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-09-10 07:57:52 -0300

    refactor: Update app title and home page title

--- lib/features/form/presentation/screens/form_screen.dart
+++ lib/features/form/presentation/screens/form_screen.dart
@@ -27,7 +27,7 @@ class _FormScreenState extends State<FormScreen> {
       backgroundColor: MyApp.themeHelper.backgroundColor,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('App da Fer'),
+        title: Text('Refletir Ações pelo clima'),
       ),
       body: Stack(
         children: [
@@ -37,156 +37,172 @@ class _FormScreenState extends State<FormScreen> {
               valueListenable: controller.valueListenable,
               builder: (context, state, __) {
                 return Center(
-                  child: SizedBox(
-                    width: 350,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Card(
-                          color: Theme.of(context)
-                              .colorScheme
-                              .surface
-                              .withOpacity(0.9),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              controller.state.formDatabinding.defaultForm
-                                  .firstWhere((element) =>
-                                      element.id ==
-                                      controller.state.currentQuestionId)
-                                  .question,
-                              style: Theme.of(context).textTheme.headlineMedium,
+                  child: SingleChildScrollView(
+                    child: SizedBox(
+                      width: 350,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Card(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .surface
+                                .withOpacity(0.9),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                controller.state.formDatabinding.defaultForm
+                                    .firstWhere((element) =>
+                                        element.id ==
+                                        controller.state.currentQuestionId)
+                                    .question,
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                              ),
                             ),
                           ),
-                        ),
-                        Card(
-                          color: Theme.of(context)
-                              .colorScheme
-                              .surface
-                              .withOpacity(0.9),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListView.builder(
-                              itemCount: controller
-                                  .state.formDatabinding.defaultForm
-                                  .firstWhere((element) =>
-                                      element.id ==
-                                      controller.state.currentQuestionId)
-                                  .options
-                                  ?.length,
-                              shrinkWrap: true,
-                              itemBuilder: (context, index) {
-                                if (controller.state.formDatabinding.defaultForm
-                                        .firstWhere((element) =>
-                                            element.id ==
-                                            controller.state.currentQuestionId)
-                                        .type ==
-                                    QuestionTypeEnum.multipleResponse) {
-                                  return CheckboxListTile(
-                                    value: controller.state.answers
-                                        .firstWhere((element) =>
-                                            element.questionId ==
-                                            controller.state.currentQuestionId)
-                                        .answers
-                                        .contains(
-                                          controller
+                          Card(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .surface
+                                .withOpacity(0.9),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ListView.builder(
+                                itemCount: controller
+                                    .state.formDatabinding.defaultForm
+                                    .firstWhere((element) =>
+                                        element.id ==
+                                        controller.state.currentQuestionId)
+                                    .options
+                                    ?.length,
+                                shrinkWrap: true,
+                                itemBuilder: (context, index) {
+                                  if (controller
+                                          .state.formDatabinding.defaultForm
+                                          .firstWhere((element) =>
+                                              element.id ==
+                                              controller
+                                                  .state.currentQuestionId)
+                                          .type ==
+                                      QuestionTypeEnum.multipleResponse) {
+                                    return CheckboxListTile(
+                                      value: controller.state.answers
+                                          .firstWhere((element) =>
+                                              element.questionId ==
+                                              controller
+                                                  .state.currentQuestionId)
+                                          .answers
+                                          .contains(
+                                            controller.state.formDatabinding
+                                                .defaultForm
+                                                .firstWhere((element) =>
+                                                    element.id ==
+                                                    controller.state
+                                                        .currentQuestionId)
+                                                .options![index]
+                                                .id,
+                                          ),
+                                      onChanged: (value) =>
+                                          controller.onChangeCheckbox(controller
                                               .state.formDatabinding.defaultForm
                                               .firstWhere((element) =>
                                                   element.id ==
                                                   controller
                                                       .state.currentQuestionId)
                                               .options![index]
-                                              .id,
-                                        ),
-                                    onChanged: (value) =>
-                                        controller.onChangeCheckbox(controller
-                                            .state.formDatabinding.defaultForm
-                                            .firstWhere((element) =>
-                                                element.id ==
-                                                controller
-                                                    .state.currentQuestionId)
-                                            .options![index]
-                                            .id),
-                                    title: Text(controller
-                                        .state.formDatabinding.defaultForm
-                                        .firstWhere((element) =>
-                                            element.id ==
-                                            controller.state.currentQuestionId)
-                                        .options![index]
-                                        .option),
-                                  );
-                                }
-                                if (controller.state.formDatabinding.defaultForm
-                                        .firstWhere((element) =>
-                                            element.id ==
-                                            controller.state.currentQuestionId)
-                                        .type ==
-                                    QuestionTypeEnum.multipleChoice) {
-                                  return RadioListTile<int>(
-                                    title: Text(controller
-                                        .state.formDatabinding.defaultForm
-                                        .firstWhere((element) =>
-                                            element.id ==
-                                            controller.state.currentQuestionId)
-                                        .options![index]
-                                        .option),
-                                    value: controller
-                                        .state.formDatabinding.defaultForm
-                                        .firstWhere((element) =>
-                                            element.id ==
-                                            controller.state.currentQuestionId)
-                                        .options![index]
-                                        .id,
-                                    groupValue: controller.state.answers
-                                        .firstWhere((element) =>
-                                            element.questionId ==
-                                            controller.state.currentQuestionId)
-                                        .answers
-                                        .first,
-                                    onChanged: controller.onChangeRadio,
-                                  );
-                                }
-                                return const SizedBox();
-                              },
+                                              .id),
+                                      title: Text(controller
+                                          .state.formDatabinding.defaultForm
+                                          .firstWhere((element) =>
+                                              element.id ==
+                                              controller
+                                                  .state.currentQuestionId)
+                                          .options![index]
+                                          .option),
+                                    );
+                                  }
+                                  if (controller
+                                          .state.formDatabinding.defaultForm
+                                          .firstWhere((element) =>
+                                              element.id ==
+                                              controller
+                                                  .state.currentQuestionId)
+                                          .type ==
+                                      QuestionTypeEnum.multipleChoice) {
+                                    return RadioListTile<int>(
+                                      title: Text(controller
+                                          .state.formDatabinding.defaultForm
+                                          .firstWhere((element) =>
+                                              element.id ==
+                                              controller
+                                                  .state.currentQuestionId)
+                                          .options![index]
+                                          .option),
+                                      value: controller
+                                          .state.formDatabinding.defaultForm
+                                          .firstWhere((element) =>
+                                              element.id ==
+                                              controller
+                                                  .state.currentQuestionId)
+                                          .options![index]
+                                          .id,
+                                      groupValue: controller.state.answers
+                                          .firstWhere((element) =>
+                                              element.questionId ==
+                                              controller
+                                                  .state.currentQuestionId)
+                                          .answers
+                                          .first,
+                                      onChanged: controller.onChangeRadio,
+                                    );
+                                  }
+                                  return const SizedBox();
+                                },
+                              ),
                             ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            controller.state.answers
-                                        .where((element) =>
-                                            element.questionId ==
-                                            controller.state.currentQuestionId)
-                                        .first
-                                        .answers
-                                        .contains(0) ||
-                                    controller.state.answers
-                                        .where((element) =>
-                                            element.questionId ==
-                                            controller.state.currentQuestionId)
-                                        .first
-                                        .answers
-                                        .isEmpty
-                                ? null
-                                : controller.state.currentQuestionId == 45
-                                    ? Navigator.of(context, rootNavigator: true)
-                                        .push(
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const ResultsScreen(),
-                                        ),
-                                      )
-                                    : controller.nextQuestion();
-                          },
-                          child: const Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('Próxima pergunta'),
-                              Icon(Icons.skip_next_rounded),
-                            ],
+                          ElevatedButton(
+                            onPressed: () {
+                              controller.state.answers
+                                          .where((element) =>
+                                              element.questionId ==
+                                              controller
+                                                  .state.currentQuestionId)
+                                          .first
+                                          .answers
+                                          .contains(0) ||
+                                      controller.state.answers
+                                          .where((element) =>
+                                              element.questionId ==
+                                              controller
+                                                  .state.currentQuestionId)
+                                          .first
+                                          .answers
+                                          .isEmpty
+                                  ? null
+                                  : controller.state.currentQuestionId == 45
+                                      ? Navigator.of(context,
+                                              rootNavigator: true)
+                                          .push(
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                const ResultsScreen(),
+                                          ),
+                                        )
+                                      : controller.nextQuestion();
+                            },
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('Próxima pergunta'),
+                                Icon(Icons.skip_next_rounded),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                          const SizedBox(height: 30),
+                        ],
+                      ),
                     ),
                   ),
                 );
