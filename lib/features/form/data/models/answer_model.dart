09139389ddeccb63724fd58ca1b0b22d4cb7e9d7
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-11-04 00:12:14 -0300

    Add keywords field to AnswerModel and update copyWith method

--- lib/features/form/data/models/answer_model.dart
+++ lib/features/form/data/models/answer_model.dart
@@ -1,18 +1,22 @@
 class AnswerModel {
   final int questionId;
   final List<int> answers;
+  final List<String> keywords;
   AnswerModel({
     required this.questionId,
     required this.answers,
+    required this.keywords,
   });
 
   AnswerModel copyWith({
     int? questionId,
     List<int>? answers,
+    List<String>? keywords,
   }) {
     return AnswerModel(
       questionId: questionId ?? this.questionId,
       answers: answers ?? this.answers,
+      keywords: keywords ?? this.keywords,
     );
   }
 }
