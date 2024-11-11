873116d11da5f8625312eefe245c5d18a4103577
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-11-10 22:04:31 -0300

    Add keyword visualization and localization to ResultsScreen

--- lib/features/results/presentation/results_screen.dart
+++ lib/features/results/presentation/results_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:word_cloud/word_cloud.dart';
 
 import '../../../helpers/background_widget.dart';
+import '../../../l10n/localization_context.dart';
 import '../../../main.dart';
 
 class ResultsScreen extends StatefulWidget {
-  const ResultsScreen({super.key});
+  final List<Map<dynamic, dynamic>> keywords;
+  const ResultsScreen({required this.keywords, super.key});
 
   @override
   State<ResultsScreen> createState() => _ResultsScreenState();
@@ -12,12 +15,9 @@ class ResultsScreen extends StatefulWidget {
 
 class _ResultsScreenState extends State<ResultsScreen> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    print(widget.keywords);
     return Scaffold(
       backgroundColor: MyApp.themeHelper.backgroundColor,
       appBar: AppBar(
@@ -29,21 +29,106 @@ class _ResultsScreenState extends State<ResultsScreen> {
           BackgroundWidget(
               image: MyApp.themeHelper.image, credit: MyApp.themeHelper.credit),
           Center(
-            child: SizedBox(
-              width: 350,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "Obrigado por testar",
-                        style: Theme.of(context).textTheme.headlineMedium,
+            child: SingleChildScrollView(
+              child: SizedBox(
+                width: size.width * 0.95,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    InteractiveViewer(
+                      child: Card(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .surface
+                            .withOpacity(0.9),
+                        child: WordCloudView(
+                          data: WordCloudData(data: widget.keywords),
+                          mapwidth: size.width * 0.95,
+                          mapheight: size.width * 0.95,
+                          mintextsize: 10,
+                          maxtextsize: 30,
+                          shape: WordCloudCircle(radius: 175),
+                          colorlist: [
+                            Theme.of(context).colorScheme.primary,
+                            Theme.of(context).colorScheme.secondary,
+                            Theme.of(context).colorScheme.tertiary,
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    Card(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .surface
+                          .withOpacity(0.9),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              '•${Localization.tr.result1}',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              textAlign: TextAlign.justify,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              '•${Localization.tr.result2}',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              textAlign: TextAlign.justify,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              '•${Localization.tr.result3}',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              textAlign: TextAlign.justify,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              '•${Localization.tr.result4}',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              textAlign: TextAlign.justify,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              Localization.tr.result_question,
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              textAlign: TextAlign.justify,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.book),
+                              Text(Localization.tr.result_option1),
+                            ],
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context, rootNavigator: true)
+                                .popUntil((route) => route.isFirst);
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.home),
+                              Text(Localization.tr.result_option2),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 50),
+                  ],
+                ),
               ),
             ),
           ),
