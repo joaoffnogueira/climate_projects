d3ff14c19d86a28ab287456b06abb2f41b557880
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-11-10 22:04:14 -0300

    Add MyHomePage widget with navigation and drawer functionality

---
+++ lib/features/home/presentation/home_screen.dart
@@ -0,0 +1,87 @@
+import 'package:climate_change_projects/main.dart';
+import 'package:flutter/material.dart';
+
+import '../../../helpers/background_widget.dart';
+import '../../form/presentation/screens/form_screen.dart';
+import 'widgets/history_drawer.dart';
+import 'widgets/info_drawer.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: MyApp.themeHelper.backgroundColor,
+      drawer: InfoDrawer(),
+      endDrawer: HistoryDrawer(),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info_outline),
+            onPressed: () {
+              _scaffoldKey.currentState!.openDrawer();
+            },
+          ),
+          const SizedBox(width: 10),
+          IconButton(
+            icon: const Icon(Icons.book),
+            onPressed: () {},
+          ),
+          const SizedBox(width: 10),
+          IconButton(
+            icon: const Icon(Icons.history),
+            onPressed: () {
+              _scaffoldKey.currentState!.openEndDrawer();
+            },
+          ),
+          const SizedBox(width: 10),
+        ],
+      ),
+      body: Stack(
+        children: [
+          BackgroundWidget(
+              image: MyApp.themeHelper.image, credit: MyApp.themeHelper.credit),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                SizedBox(
+                  width: 350,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context, rootNavigator: true).push(
+                        MaterialPageRoute(
+                          builder: (context) => const FormScreen(),
+                        ),
+                      );
+                    },
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.add),
+                        Text('Nova reflexão'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
