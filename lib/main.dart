02c37085bd2efc2c86e7d8b42f4fe732d9a130ba
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-11-10 22:04:05 -0300

    Refactor main.dart to remove MyHomePage and replace FormScreen with HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:climate_change_projects/helpers/background_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'features/form/presentation/screens/form_screen.dart';
+import 'features/home/presentation/home_screen.dart';
 import 'helpers/theme_helper.dart';
 import 'l10n/app_localizations.dart';
 
@@ -38,80 +37,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'EmergeClima'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: MyApp.themeHelper.backgroundColor,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.info_outline),
-              onPressed: () {},
-            ),
-            const SizedBox(width: 10),
-            IconButton(
-              icon: const Icon(Icons.book),
-              onPressed: () {},
-            ),
-            const SizedBox(width: 10),
-            IconButton(
-              icon: const Icon(Icons.history),
-              onPressed: () {},
-            ),
-            const SizedBox(width: 10),
-          ],
-        ),
-        body: Stack(
-          children: [
-            BackgroundWidget(
-                image: MyApp.themeHelper.image,
-                credit: MyApp.themeHelper.credit),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  SizedBox(
-                    width: 350,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context, rootNavigator: true).push(
-                          MaterialPageRoute(
-                            builder: (context) => const FormScreen(),
-                          ),
-                        );
-                      },
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.add),
-                          Text('Nova reflexão'),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
