373e25d78ef1bce357a83089992d71203c851746
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-11-15 10:39:56 -0300

    Add LibraryScreen with section cards for navigation to SearchScreen

---
+++ lib/features/library/presentation/library_screen.dart
@@ -0,0 +1,92 @@
+import 'package:climate_change_projects/features/library/widgets/section_card.dart';
+import 'package:flutter/material.dart';
+
+import '../../../helpers/background_widget.dart';
+import '../../../main.dart';
+import 'search_screen.dart';
+
+class LibraryScreen extends StatefulWidget {
+  const LibraryScreen({super.key});
+
+  @override
+  State<LibraryScreen> createState() => _LibraryScreenState();
+}
+
+class _LibraryScreenState extends State<LibraryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      backgroundColor: MyApp.themeHelper.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Biblioteca'),
+      ),
+      body: Stack(
+        children: [
+          BackgroundWidget(
+              image: MyApp.themeHelper.image, credit: MyApp.themeHelper.credit),
+          Center(
+            child: SingleChildScrollView(
+              child: SizedBox(
+                width: size.width * 0.95,
+                height: size.height * 0.90,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    SectionCard(
+                      onPressed: () {
+                        Navigator.of(context, rootNavigator: true).push(
+                          MaterialPageRoute(
+                            builder: (context) => const SearchScreen(),
+                          ),
+                        );
+                      },
+                      title: 'Diagnóstico',
+                      icon: Icons.thermostat,
+                    ),
+                    SectionCard(
+                      onPressed: () {
+                        Navigator.of(context, rootNavigator: true).push(
+                          MaterialPageRoute(
+                            builder: (context) => const SearchScreen(),
+                          ),
+                        );
+                      },
+                      title: 'Projeto',
+                      icon: Icons.note_alt_rounded,
+                    ),
+                    SectionCard(
+                      onPressed: () {
+                        Navigator.of(context, rootNavigator: true).push(
+                          MaterialPageRoute(
+                            builder: (context) => const SearchScreen(),
+                          ),
+                        );
+                      },
+                      title: 'Emergência Climática',
+                      titleStyle: Theme.of(context).textTheme.headlineSmall,
+                      icon: Icons.thunderstorm,
+                      iconSize: 120,
+                    ),
+                    SectionCard(
+                      onPressed: () {
+                        Navigator.of(context, rootNavigator: true).push(
+                          MaterialPageRoute(
+                            builder: (context) => const SearchScreen(),
+                          ),
+                        );
+                      },
+                      title: 'Avaliação',
+                      icon: Icons.checklist_rounded,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
