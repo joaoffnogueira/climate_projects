7af5a13c4c306614ea51c037c1c225c449cd5d75
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-11-17 19:14:11 -0300

    Refactor HistoryDrawer to use state management and integrate Realm for history data retrieval

--- lib/features/home/presentation/widgets/history_drawer.dart
+++ lib/features/home/presentation/widgets/history_drawer.dart
@@ -1,26 +1,71 @@
 import 'package:flutter/material.dart';
 
-import '../../../../l10n/localization_context.dart';
+import '../../../../core/db_local/db_local.dart';
+import '../../../../core/db_local/db_local_realm.dart';
+import '../../../form/data/models/history_model.dart';
+import '../../../results/presentation/results_screen.dart';
 
-class HistoryDrawer extends StatelessWidget {
+class HistoryDrawer extends StatefulWidget {
   const HistoryDrawer({super.key});
+
+  @override
+  State<HistoryDrawer> createState() => _HistoryDrawerState();
+}
+
+class _HistoryDrawerState extends State<HistoryDrawer> {
+  final DbLocal dbLocal = DbLocalRealm([HistoryModel.schema]);
+  late List<HistoryModel> history;
+
+  @override
+  void initState() {
+    super.initState();
+    dbLocal.openConection();
+    history = dbLocal.all<HistoryModel>();
+  }
+
+  @override
+  void dispose() {
+    dbLocal.close();
+    super.dispose();
+  }
+
+  List<Map> _keywordsFromString(String string) {
+    final List<Map> result = [];
+    final parts = string.split(' ');
+    for (var i = 0; i < parts.length - 1; i += 2) {
+      result.add({"word": parts[i], "value": int.parse(parts[i + 1])});
+    }
+    return result;
+  }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Material(
       child: SizedBox(
-        width: size.width * 0.8,
+        width: size.width * 0.5,
         height: size.height,
         child: Scaffold(
           appBar: AppBar(
-            title: Text('Histórico de análises'),
+            title: Text('Histórico'),
           ),
           body: ListView.builder(
-            itemCount: 10,
+            itemCount: history.length,
             itemBuilder: (context, index) {
+              final dateTime = DateTime.parse(history[index].title);
               return ListTile(
-                title: Text('Item $index'),
+                title: Text(
+                  '${dateTime.day}/${dateTime.month}/${dateTime.year} - ${dateTime.hour}:${dateTime.minute}',
+                ),
+                onTap: () {
+                  Navigator.of(context, rootNavigator: true).push(
+                    MaterialPageRoute(
+                      builder: (context) => ResultsScreen(
+                        keywords: _keywordsFromString(history[index].keywords),
+                      ),
+                    ),
+                  );
+                },
               );
             },
           ),
