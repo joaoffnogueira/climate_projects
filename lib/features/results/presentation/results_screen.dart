6cbc35981285c1f7e564afdfb1f48dd63c3fb28c
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-11-17 23:41:06 -0300

    Enhance ResultsScreen with AnswersWidget and recommendedKeywords display

--- lib/features/results/presentation/results_screen.dart
+++ lib/features/results/presentation/results_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:ui' as ui;
+import 'package:climate_change_projects/features/results/presentation/widgets/answers_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:gal/gal.dart';
 import 'package:word_cloud/word_cloud.dart';
 
 import '../../../helpers/background_widget.dart';
@@ -8,13 +13,20 @@ import '../../library/presentation/library_screen.dart';
 
 class ResultsScreen extends StatefulWidget {
   final List<Map<dynamic, dynamic>> keywords;
-  const ResultsScreen({required this.keywords, super.key});
+  final Set<String> recommendedKeywords;
+  final String answersFromDb;
+  const ResultsScreen(
+      {required this.keywords,
+      required this.recommendedKeywords,
+      required this.answersFromDb,
+      super.key});
 
   @override
   State<ResultsScreen> createState() => _ResultsScreenState();
 }
 
 class _ResultsScreenState extends State<ResultsScreen> {
+  final GlobalKey _widgetKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
@@ -35,23 +47,143 @@ class _ResultsScreenState extends State<ResultsScreen> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                    InteractiveViewer(
-                      child: Card(
-                        color: Theme.of(context)
-                            .colorScheme
-                            .surface
-                            .withOpacity(0.9),
-                        child: WordCloudView(
-                          data: WordCloudData(data: widget.keywords),
-                          mapwidth: size.width * 0.95,
-                          mapheight: size.width * 0.95,
-                          mintextsize: 10,
-                          maxtextsize: 30,
-                          shape: WordCloudCircle(radius: 175),
-                          colorlist: [
-                            Theme.of(context).colorScheme.primary,
-                            Theme.of(context).colorScheme.secondary,
-                            Theme.of(context).colorScheme.tertiary,
+                    Card(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .surface
+                          .withOpacity(0.9),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Pontos fortes do seu projeto',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          RepaintBoundary(
+                            key: _widgetKey,
+                            child: WordCloudView(
+                              data: WordCloudData(data: widget.keywords),
+                              mapwidth: size.width * 0.95,
+                              mapheight: size.width * 0.95,
+                              mintextsize: 10,
+                              maxtextsize: 30,
+                              shape: WordCloudCircle(radius: 175),
+                              colorlist: [
+                                Theme.of(context).colorScheme.primary,
+                                Theme.of(context).colorScheme.secondary,
+                                Theme.of(context).colorScheme.tertiary,
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text('Respostas'),
+                                    content: AnswersWidget(
+                                        answersFromDb: widget.answersFromDb),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('Fechar'),
+                                      ),
+                                    ],
+                                  );
+                                });
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.check_box_rounded),
+                              Text('Ver Respostas'),
+                            ],
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            try {
+                              final dateTime = DateTime.now();
+                              RenderRepaintBoundary boundary =
+                                  _widgetKey.currentContext!.findRenderObject()
+                                      as RenderRepaintBoundary;
+                              ui.Image image =
+                                  await boundary.toImage(pixelRatio: 10.0);
+                              ByteData? byteData = await image.toByteData(
+                                  format: ui.ImageByteFormat.png);
+                              Uint8List pngBytes =
+                                  byteData!.buffer.asUint8List();
+                              var hasAccess = await Gal.hasAccess();
+                              !hasAccess
+                                  ? await Gal.requestAccess().then((value) {
+                                      if (value) {
+                                        Gal.putImageBytes(pngBytes,
+                                            name:
+                                                'IMG-${dateTime.day}-${dateTime.month}-${dateTime.year}-${dateTime.hour}h${dateTime.minute}.png');
+                                      } else {
+                                        throw 'Permissão negada';
+                                      }
+                                    })
+                                  : await Gal.putImageBytes(pngBytes,
+                                      name:
+                                          'IMG-${dateTime.day}-${dateTime.month}-${dateTime.year}-${dateTime.hour}h${dateTime.minute}.png');
+
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Imagem salva na galeria'),
+                                ),
+                              );
+                            } catch (e) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Permissão negada'),
+                                ),
+                              );
+                            }
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.download_rounded),
+                              Text('Baixar imagem'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Card(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .surface
+                          .withOpacity(0.9),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Temas recomendados para aprofundamento:',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              widget.recommendedKeywords.join(', '),
+                              maxLines: 30,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium
+                                  ?.copyWith(
+                                    color: Theme.of(context).colorScheme.error,
+                                  ),
+                              textAlign: TextAlign.center,
+                            ),
                           ],
                         ),
                       ),
