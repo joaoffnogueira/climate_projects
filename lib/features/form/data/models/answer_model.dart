9285aedc51020e4b91a4bfe3bc6a39d38a466e40
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-11-18 08:33:15 -0300

    Refactor AnswerModel to include questionText and answerTexts, and update copyWith method

--- lib/features/form/data/models/answer_model.dart
+++ lib/features/form/data/models/answer_model.dart
@@ -1,36 +1,31 @@
 class AnswerModel {
   final int questionId;
+  String questionText;
   final List<int> answers;
+  final List<String> answerTexts;
   final List<String> keywords;
   AnswerModel({
     required this.questionId,
+    required this.questionText,
     required this.answers,
+    required this.answerTexts,
     required this.keywords,
   });
 
   AnswerModel copyWith({
     int? questionId,
+    String? questionText,
     List<int>? answers,
+    List<String>? answerTexts,
     List<String>? keywords,
   }) {
     return AnswerModel(
       questionId: questionId ?? this.questionId,
+      questionText: questionText ?? this.questionText,
       answers: answers ?? this.answers,
+      answerTexts: answerTexts ?? this.answerTexts,
       keywords: keywords ?? this.keywords,
     );
   }
 
-  @override
-  String toString() {
-    return '$questionId ${answers.join(',')} ${keywords.join(',')}';
-  }
-
-  factory AnswerModel.fromString(String string) {
-    final parts = string.split(' ');
-    return AnswerModel(
-      questionId: int.parse(parts[0]),
-      answers: parts[1].split(',').map(int.parse).toList(),
-      keywords: parts[2].split(','),
-    );
-  }
 }
