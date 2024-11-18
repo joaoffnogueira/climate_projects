c895802789e6e3d205d1a63f9db0414bcff37c62
joaoffnogueira <dev.joaonogueira@gmail.com>
2024-11-18 08:33:53 -0300

    Refactor AnswersWidget to change answersFromDb type to Map<String, String> and update ListView.builder to display answers accordingly

--- lib/features/results/presentation/widgets/answers_widget.dart
+++ lib/features/results/presentation/widgets/answers_widget.dart
@@ -1,41 +1,24 @@
-import '../../../form/data/models/answer_model.dart';
 import 'package:flutter/material.dart';
 
-import '../../../form/data/models/tense_enum.dart';
-import '../../../form/presentation/stores/form_databinding.dart';
-
 class AnswersWidget extends StatelessWidget {
-  final String answersFromDb;
+  final Map<String, String> answersFromDb;
   const AnswersWidget({required this.answersFromDb, super.key});
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
-    final questions = FormDatabinding(TenseEnum.present).defaultForm;
-    final answers = List<AnswerModel>.from(
-        answersFromDb.split('/').map((e) => AnswerModel.fromString(e)));
+    final answers = Map<String, String>.from(answersFromDb);
+    answers.removeWhere((key, value) => key == '');
     return SizedBox(
       width: size.width * 0.95,
       height: size.height * 0.95,
       child: ListView.builder(
         shrinkWrap: true,
-        itemCount: questions.length,
+        itemCount: answers.length,
         itemBuilder: (context, index) {
-          final question = questions[index];
-          var answer = '';
-          for (var i = 0; i < questions.length;) {
-            if (answers[i].questionId == question?.id) {
-              for (var j = 0; j < answers[i].answers.length;) {
-                if (j > 0) {
-                  answer += ', ${answers[i].answers[j]}';
-                }
-                break;
-              }
-            }
-          }
           return ListTile(
-            title: Text(question?.question ?? ''),
-            subtitle: Text(answer.isNotEmpty ? answer : 'Sem resposta'),
+            title: Text(answers.keys.elementAt(index)),
+            subtitle: Text(answers.values.elementAt(index)),
           );
         },
       ),
