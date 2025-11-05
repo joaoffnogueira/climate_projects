b89a41f843dad9dee8385b0502ddae2baf1e5304
João Nogueira <jffn@protonmail.com>
2025-11-05 06:20:02 -0300

    feat: refactor BackgroundWidget to use StatefulWidget for improved image caching

--- lib/helpers/background_widget.dart
+++ lib/helpers/background_widget.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
 
-class BackgroundWidget extends StatelessWidget {
+class BackgroundWidget extends StatefulWidget {
   final String image;
   final String credit;
   const BackgroundWidget(
       {super.key, required this.image, required this.credit});
 
   @override
+  State<BackgroundWidget> createState() => _BackgroundWidgetState();
+}
+
+class _BackgroundWidgetState extends State<BackgroundWidget> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final mediaQuery = MediaQuery.of(context);
+      final targetCacheWidth =
+          (mediaQuery.size.width * mediaQuery.devicePixelRatio).round();
+      precacheImage(
+        ResizeImage(AssetImage(widget.image), width: targetCacheWidth),
+        context,
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     final targetCacheWidth =
         (mediaQuery.size.width * mediaQuery.devicePixelRatio).round();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      precacheImage(
-        ResizeImage(AssetImage(image), width: targetCacheWidth),
-        context,
-      );
-    });
     return Stack(
       alignment: Alignment.bottomRight,
       children: [
         Image.asset(
-          image,
+          widget.image,
           fit: BoxFit.cover,
           height: double.infinity,
           width: double.infinity,
@@ -31,7 +44,7 @@ class BackgroundWidget extends StatelessWidget {
         ),
         Container(
           decoration: BoxDecoration(
-            color: Colors.black.withValues(alpha: 0.5),
+            color: const Color.fromRGBO(0, 0, 0, 0.5),
             borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(10),
             ),
@@ -40,7 +53,7 @@ class BackgroundWidget extends StatelessWidget {
             padding:
                 const EdgeInsets.symmetric(vertical: 8.0, horizontal: 40.0),
             child: Text(
-              credit,
+              widget.credit,
               maxLines: 2,
               style: Theme.of(context).textTheme.labelSmall?.copyWith(
                     color: Theme.of(context).colorScheme.onPrimary,
